21b867a9ba9ca1bbf2756fbd98952c76e8582389
Leo Gau <leo.gau1@gmail.com>
2011-02-04 22:19:55 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os
+import cgi
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp import template
+
+class MainHandler(webapp.RequestHandler):
+    def get(self):
+			path = os.path.join(os.path.dirname(__file__), 'index.html')
+			template_values = {}
+			self.response.out.write(template.render(path, template_values))
+
+class BookHandler(webapp.RequestHandler):
+	def get(self):
+		file_name = self.request.path.replace('/', '') + '.html'
+		path = os.path.join(os.path.dirname(__file__), file_name)
+		template_values = {}
+		self.response.out.write(template.render(path, template_values))
+
+def main():
+    application = webapp.WSGIApplication([('/', MainHandler),
+                                          ('/.*', BookHandler)],
+                                         debug=True)
+    util.run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
